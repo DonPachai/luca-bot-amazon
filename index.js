57d26e2ea6a3b55fc75b9a2820a39ef841c48592
DonPachai <33960993+DonPachai@users.noreply.github.com>
2022-11-17 11:47:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ Contributers:
  - Nitesh Sahni (@nsniteshsahni)
 License: MIT
 */
-
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
 const TelegramBot = require("node-telegram-bot-api");
 const fetch = require("node-fetch");
 
